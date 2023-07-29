8ff4881b3e8799b680342905d2f8ac0803d6b669
Pablo Domínguez <pablo.developer0@gmail.com>
2023-07-29 18:08:44 +0200

    Paquetes  bootstrap react-bootstrap react-icons Componente Header creado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
 const App = () => {
-  return <h2>Welcome to eCommerce</h2>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
