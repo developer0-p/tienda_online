72119b01b7f7825a8d848563579bee4c7d9fe38a
Pablo Domínguez <pablo.developer0@gmail.com>
2023-08-15 10:47:33 +0200

    Express Server & Initial Products Routes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-import express from 'express';
-const port = 5000
+import express from "express";
+const port = 5000;
+import products from "./data/products.js";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-})
+  console.log(`Server is running at http://localhost:${port}`);
+});
