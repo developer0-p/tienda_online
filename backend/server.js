2b38d15e5de33ef44bd65f47fc4e371fad15d4c4
Pablo <pablo.developer0@gmail.com>
2023-08-23 18:30:21 +0200

    Product, User and Order Models added

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import connectDB from "./config/db.js";
-import products from "./data/products.js";
-const port = process.env.PORT || 5000;
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './config/db.js'
+import products from './data/products.js'
+const port = process.env.PORT || 5000
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at port ${port}`)
+})
