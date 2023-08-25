3e2923260eded4d5d655bdb2277c37f7a1c19e51
developer0-p <84157163+developer0-p@users.noreply.github.com>
2023-08-25 09:28:04 +0000

    Controller added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
+import {notFound, errorHandler} from './middleware/errorHandler.js'
+
 
 import productRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000
@@ -15,6 +17,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`)
