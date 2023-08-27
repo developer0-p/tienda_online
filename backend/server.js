196dee8b612b1a0a4d4a192b92539d3bfc306add
Pablo Domínguez <pablo.developer0@gmail.com>
2023-08-27 11:31:20 +0200

    server.js prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,42 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import connectDB from './config/db.js'
-import {notFound, errorHandler} from './middleware/errorHandler.js'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
+import path from "path";
 
+import productRoutes from "./routes/productRoutes.js";
+const port = process.env.PORT || 5000;
 
-import productRoutes from './routes/productRoutes.js'
-const port = process.env.PORT || 5000
+connectDB();
 
-connectDB()
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+app.use("/api/products", productRoutes);
 
-app.use('/api/products', productRoutes)
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.use(notFound)
-app.use(errorHandler)
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server is running at port ${port}`)
-})
+  console.log(`Server is running at port ${port}`);
+});
