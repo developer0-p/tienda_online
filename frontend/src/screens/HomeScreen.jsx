59432393a7e20b9a1ccfd520e7a702f7b7476f24
developer0-p <84157163+developer0-p@users.noreply.github.com>
2023-08-29 14:44:52 +0000

    get product detail API Slice

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,28 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Products'
-import axios from 'axios'
+
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+  const { data: products, isLoading, error } = useGetProductsQuery()
   return (
     <>
-      <h1>Últimas Novedades (data from atlas Mongodb)</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      { isLoading ? (
+      <h2>Loading...</h2>
+      ) : error ? (<div>{ error?.data?.message || error.error }</div>) : (
+        <>
+        <h1>Últimas Novedades (api slices)</h1>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+        
+      </>
+      ) }
     </>
   )
 }
