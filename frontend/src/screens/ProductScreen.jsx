8ac496c52e4d6c2b5009273b790627db10cb3390
developer0-p <84157163+developer0-p@users.noreply.github.com>
2023-08-29 15:04:38 +0000

    Loader & Message Components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const ProductScreen = () => {
   const { id: productId } = useParams()
@@ -13,11 +15,10 @@ const ProductScreen = () => {
     <Link className='btn btn-light my-3' to='/'>
         Volver
       </Link>
+      
       { isLoading ? (
-      <h2>Loading...</h2>
-      ) : error ? (
-        <div>{ error?.data?.message || error.error }</div>
-      ) : (
+      <Loader />
+      ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (
       <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
