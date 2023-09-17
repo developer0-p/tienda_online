745c42cd75be45c19bb603fa916da3611e8a4732
Pablo <pablo.developer0@gmail.com>
2023-09-17 12:22:15 +0200

    user routes & controller

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,26 +4,28 @@ import Product from '../components/Products'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery()
   return (
     <>
-      { isLoading ? (
-      <Loader />
-      ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
         <>
-        <h1>Últimas Novedades (shopping cart functionality)</h1>
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-        
-      </>
-      ) }
+          <h1>Últimas Novedades (backend authentication)</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   )
 }
