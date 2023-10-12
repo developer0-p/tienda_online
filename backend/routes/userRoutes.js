afd1fe25a96973fd6fb902f90bf27ce620242c16
developer0-p <84157163+developer0-p@users.noreply.github.com>
2023-10-12 11:06:58 +0200

    auth middleware endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,11 +11,12 @@ import {
     getUsersById,
     updateUser
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser).get(protect, admin,getUsers);
 router.post("/logout", logoutUser);
-router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUsersById).put(updateUser);
+router.post("/auth", authUser);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin,deleteUser).get(getUsersById).put(protect,admin,updateUser);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorHandler.js'
@@ -15,6 +16,8 @@ const app = express()
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
