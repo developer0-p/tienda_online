f064613920d15f80a25f86caa4c2485689fc7087
developer0-p <84157163+developer0-p@users.noreply.github.com>
2023-11-12 10:31:14 +0100

    Login screen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -17,11 +17,31 @@ const LoginScreen = () => {
         <h1>Inicia sesión</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='email' className='my-3'>
-
+              <Form.Label>Correo electrónico</Form.Label>
+              <Form.Control 
+                type='email'
+                placeholder='Introduce tu dirección de correo electrónico'
+                value={email}
+                onChange={(e)=> setEmail(e.target.value)}></Form.Control>
             </Form.Group>
-            <Form.Group controlId='password' className='my-3'>
 
-        </Form.Group>
+            <Form.Group controlId='password' className='my-3'>
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control 
+                type='password'
+                placeholder='Introduce tu contraseña'
+                value={password}
+                onChange={(e)=> setPassword(e.target.value)}>
+
+                </Form.Control>
+            </Form.Group>
+            <Button type='submit' variant='primary' className='mt-2'>Iniciar sesión</Button>
+        </Form>
+        <Row className='py-3'>
+          <Col>
+            ¿Eres nuevo? <Link to={'/register'}>Registrate</Link>
+          </Col>
+        </Row>
     </FormContainer>
   )
 }
