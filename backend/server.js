6681f36f4f3cb0e2ef7b24a9635c05dd21e2f08f
developer0-p <84157163+developer0-p@users.noreply.github.com>
2024-01-07 17:16:33 +0100

    multer & image upload endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
-import path from "path";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
