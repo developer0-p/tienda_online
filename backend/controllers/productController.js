8a1a37b2c8ec0b196f6671ea55b17df63d3a14be
Pablo <pablo.developer0@gmail.com>
2024-01-25 19:20:37 +0100

    Create reviews backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -84,10 +84,52 @@ const deleteProduct = asyncHandler(async (req, res) => {
   res.json(updatedProduct)
 })
 
+// @desc    Create a review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    )
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Producto ya valorado')
+    }
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    }
+
+    product.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: 'Valoración añadida' })
+  } else {
+    res.status(404)
+    throw new Error('Producto no encontrado')
+  }
+  const updatedProduct = await product.save()
+  res.json(updatedProduct)
+})
+
 export {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 }
